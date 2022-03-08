e1ef467eba6eaedba8ccd70fe70a3003ca9aa009
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-08 13:55:17 +0200

    Update game component

--- 02-Components/demo-project/src/app/game/game.component.ts
+++ 02-Components/demo-project/src/app/game/game.component.ts
@@ -1,5 +1,10 @@
 import { Component } from "@angular/core";
 
+interface Game {
+    title: string,
+    price: number,
+}
+
 @Component({
     selector: 'su-game',
     // template:`<h2>This is game component</h2>`
@@ -7,5 +12,9 @@ import { Component } from "@angular/core";
     styleUrls: ['./game.component.css'],
 })
 export class GameComponent {
-
+    games: Game[] = [
+        { title: 'Minecraft', price: 10 },
+        { title: 'Candy Crush', price: 0 },
+        { title: 'Counter Strike', price: 120 },
+    ]
 }
