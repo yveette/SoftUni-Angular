cba4a66555432573582d869d7d38c8bc064ce3dd
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-08 13:41:52 +0200

    Add game component

---
+++ 02-Components/demo-project/src/app/game/game.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'su-game',
+    // template:`<h2>This is game component</h2>`
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.css'],
+})
+export class GameComponent {
+
+}
