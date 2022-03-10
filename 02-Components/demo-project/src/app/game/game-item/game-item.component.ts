c60fa6c164888d4db8e6d568f552ad4f827853dd
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-10 15:54:30 +0200

    Add more functionality

--- 02-Components/demo-project/src/app/game/game-item/game-item.component.ts
+++ 02-Components/demo-project/src/app/game/game-item/game-item.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit, Input, OnChanges, SimpleChanges, DoCheck } from "@angular/core";
+
+export interface Game {
+    title: string,
+    price: number,
+    img: string
+}
 
 @Component({
     selector: 'su-game-item',
     templateUrl: './game-item.component.html'
 })
 
-export class GameItemComponent implements OnInit, OnDestroy {
-    game: any;
+export class GameItemComponent implements OnInit, OnDestroy, OnChanges, DoCheck {
+    @Input() game: any;
 
     ngOnInit(): void {
         console.log('GameItemComponent created!');
+    }
+
+    ngDoCheck(): void {
+        console.log('GameItemComponent checking!');
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('GameItemComponent input changes!', changes);
     }
 
     ngOnDestroy(): void {
--- 02-Components/demo-project/src/app/game/game.component.ts
+++ 02-Components/demo-project/src/app/game/game.component.ts
@@ -1,10 +1,5 @@
 import { Component } from "@angular/core";
-
-interface Game {
-    title: string,
-    price: number,
-    img: string
-}
+import { Game } from "./game-item/game-item.component";
 
 @Component({
     selector: 'su-game',
@@ -14,27 +9,33 @@ interface Game {
 })
 export class GameComponent {
     shouldPriceByRed: boolean = false;
-    shouldShowGameItemComponent:boolean  = false;
+    shouldShowGameItemComponent: boolean = false;
     searchText: string = 'Game';
 
     games: Game[] = [
         { title: 'Minecraft', price: 10, img: 'https://cdn.pixabay.com/photo/2013/07/12/19/25/minecraft-154749__480.png' },
         { title: 'Candy Crush', price: 0, img: 'https://apkmagic.com.ar/wp-content/uploads/2019/05/Candy-Crush-Saga-v1.150.1.2-Mod-APK.png' },
-        { title: 'Counter Strike', price: 120 , img: 'https://gameday.bg/wp-content/uploads/2021/01/counter-strike-global-offensive-bezplatna-01.jpg'},
+        { title: 'Counter Strike', price: 120, img: 'https://gameday.bg/wp-content/uploads/2021/01/counter-strike-global-offensive-bezplatna-01.jpg' },
     ]
 
-    handleExpandContentClick(gamesContainer: HTMLElement) :void{
+    constructor() {
+        for (const game of this.games) {
+            console.log(game.title?.length);
+        }
+    }
+
+    handleExpandContentClick(gamesContainer: HTMLElement): void {
         this.shouldPriceByRed = this.shouldPriceByRed ? false : true;
         // this.shouldPriceByRed = !this.shouldPriceByRed;
 
         console.log(gamesContainer.children);
     }
 
-    handleSearchChange(event: Event){
+    handleSearchChange(event: Event) {
         console.log(event)
     }
 
-    handleCreateOrDestroyGameItem(): void{
+    handleCreateOrDestroyGameItem(): void {
         this.shouldShowGameItemComponent = !this.shouldShowGameItemComponent;
     }
 }
