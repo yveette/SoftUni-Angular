32ee608e924c5d5cd47243bdb16cc33cf8900b15
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-14 19:44:38 +0200

    Add components - header, theme-list, aside and footer

--- 04-Workshop-Components/forum/src/app/app.module.ts
+++ 04-Workshop-Components/forum/src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ThemeListComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class AppModule { }
