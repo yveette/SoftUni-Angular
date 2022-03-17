a5187059933f9989614104c5bf0c540b461ad42b
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-17 22:07:20 +0200

    Add home page with routing

--- 05-Modules-and-Routing/modules-and-routing/src/app/app.module.ts
+++ 05-Modules-and-Routing/modules-and-routing/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { CustomerModule } from './customer/customer.module';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    AboutComponent
   ],
   imports: [
     BrowserModule,
