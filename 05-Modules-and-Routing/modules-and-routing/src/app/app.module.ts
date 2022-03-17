2f0a8fa612af7aeeb76291760b9d4f692f06d751
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-17 20:02:36 +0200

    Add header

--- 05-Modules-and-Routing/modules-and-routing/src/app/app.module.ts
+++ 05-Modules-and-Routing/modules-and-routing/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { CustomerModule } from './customer/customer.module';
 
 @NgModule({
@@ -14,7 +15,8 @@ import { CustomerModule } from './customer/customer.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    CustomerModule
+    CustomerModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
