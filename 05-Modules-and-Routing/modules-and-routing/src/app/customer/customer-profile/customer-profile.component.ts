7cf68c28c54edad54d3798cf70d668adc6a251a9
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-17 22:41:15 +0200

    Add titles to pages

--- 05-Modules-and-Routing/modules-and-routing/src/app/customer/customer-profile/customer-profile.component.ts
+++ 05-Modules-and-Routing/modules-and-routing/src/app/customer/customer-profile/customer-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { CustomerService, IUser } from '../customer.service';
 
@@ -10,15 +11,23 @@ import { CustomerService, IUser } from '../customer.service';
 export class CustomerProfileComponent implements OnInit {
 
   customer!: IUser;
+  isLoading: boolean = false;
 
-  constructor(private activateRoute: ActivatedRoute, private customerService: CustomerService) { }
+  constructor(private activateRoute: ActivatedRoute,
+    private customerService: CustomerService,
+    private titleServie: Title
+  ) { }
 
   ngOnInit(): void {
     const id = this.activateRoute.snapshot.params['id'];
     // const id = this.activateRoute.snapshot.paramMap.get('id');
 
+    this.titleServie.setTitle('Profile ' + id);
+
+    this.isLoading = true;
     this.customerService.getUserById$(id).subscribe(user => {
       this.customer = user;
+      this.isLoading = false;
     })
 
   }
