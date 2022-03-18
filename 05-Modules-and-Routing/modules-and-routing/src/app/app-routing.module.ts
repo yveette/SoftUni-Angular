7b286c35405e4dea569362f989080151a29602fe
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-18 15:19:32 +0200

    Add default view

--- 05-Modules-and-Routing/modules-and-routing/src/app/app-routing.module.ts
+++ 05-Modules-and-Routing/modules-and-routing/src/app/app-routing.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerProfileComponent } from './customer/customer-profile/customer-profile.component';
 import { AboutComponent } from './pages/about/about.component';
+import { DefaultViewComponent } from './pages/default-view/default-view.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'to-be-redirected'
+  },
+  {
+    path: 'to-be-redirected',
+    component: DefaultViewComponent
+  },
   {
     path: 'home',
     component: HomeComponent
--- 05-Modules-and-Routing/modules-and-routing/src/app/app.module.ts
+++ 05-Modules-and-Routing/modules-and-routing/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { CoreModule } from './core/core.module';
 import { CustomerModule } from './customer/customer.module';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { DefaultViewComponent } from './pages/default-view/default-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    DefaultViewComponent
   ],
   imports: [
     BrowserModule,
