915212905fcc7da3cad49a7d77fb369fa9a8f502
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-23 16:53:19 +0200

    Improve theme details page with subscribe button logic

--- 06-Workshop-Modules-and-Routing/workshop-01/src/app/feature/themes/themes-detail-page/themes-detail-page.component.ts
+++ 06-Workshop-Modules-and-Routing/workshop-01/src/app/feature/themes/themes-detail-page/themes-detail-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ITheme } from 'src/app/core/interfaces';
 import { ThemeService } from 'src/app/core/theme.service';
+import { UserService } from 'src/app/core/user.service';
 
 @Component({
   selector: 'app-themes-detail-page',
@@ -11,8 +12,12 @@ import { ThemeService } from 'src/app/core/theme.service';
 export class ThemesDetailPageComponent implements OnInit {
 
   theme: ITheme;
+  isLoggedIn: boolean = this.userService.isLogged;
+  canSubscribe: boolean;
 
-  constructor(private activatedRoute: ActivatedRoute, private themeService: ThemeService) { }
+  constructor(private activatedRoute: ActivatedRoute,
+    private themeService: ThemeService,
+    private userService: UserService) { }
 
   ngOnInit(): void {
     // get param once!
@@ -26,6 +31,7 @@ export class ThemesDetailPageComponent implements OnInit {
       const themeId = params['themeId'];
       this.themeService.loadThemeById(themeId).subscribe(theme => {
         this.theme = theme;
+        this.canSubscribe = !this.theme.subscribers.includes('5fa64b162183ce1728ff371d');
       })
     })
   }
