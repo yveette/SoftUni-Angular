b05d8e26d6cdc2f3cedfd15c4f3a4923bf3fd5d7
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-23 17:07:30 +0200

    Add like and dislike buttons logic on comments

--- 06-Workshop-Modules-and-Routing/workshop-01/src/app/feature/themes/themes-detail-page/themes-detail-page.component.ts
+++ 06-Workshop-Modules-and-Routing/workshop-01/src/app/feature/themes/themes-detail-page/themes-detail-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ITheme } from 'src/app/core/interfaces';
+import { IPost, ITheme } from 'src/app/core/interfaces';
 import { ThemeService } from 'src/app/core/theme.service';
 import { UserService } from 'src/app/core/user.service';
 
@@ -34,6 +34,10 @@ export class ThemesDetailPageComponent implements OnInit {
         this.canSubscribe = !this.theme.subscribers.includes('5fa64b162183ce1728ff371d');
       })
     })
+  }
+
+  canLike(comment: IPost) {
+    return !comment.likes.includes('5fa64b162183ce1728ff371d');
   }
 
 }
