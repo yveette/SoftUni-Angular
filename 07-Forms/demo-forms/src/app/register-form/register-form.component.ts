b98ade63490251b4fece10729620a42ad0c74e5b
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-28 16:30:16 +0300

    Update form. Add ngForm and ngModel.

--- 07-Forms/demo-forms/src/app/register-form/register-form.component.ts
+++ 07-Forms/demo-forms/src/app/register-form/register-form.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent implements OnInit {
+export class RegisterFormComponent implements OnInit, AfterViewInit {
+
+  buildings: string[] = [
+    'Designer',
+    'Developer',
+    'QA'
+  ]
+
+  @ViewChild('registration') registration!: NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    // console.log(this.registration.value);
+  }
+
+  onSubmit(): void{
+    console.log(this.registration.value);
+
+  }
 }
