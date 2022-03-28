2d96e0fd35c17f943548a4bb4c8c8b57e355757b
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-28 14:51:16 +0300

    Add register form

--- 07-Forms/demo-forms/src/app/app.module.ts
+++ 07-Forms/demo-forms/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    RegisterFormComponent
+  ]
 })
 export class AppModule { }
