26aec4f358ee086dcab15f3951d129042865b039
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-03-28 20:46:53 +0300

    Add validations to register form. Show errors to user. Improve email validation.

--- 08-Workshop-Forms/workshop/src/app/auth/register/register.component.ts
+++ 08-Workshop-Forms/workshop/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { emailValidator } from '../util';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerFormGroup: FormGroup = this.formBuilder.group({
+    'username': new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    'email': new FormControl(null, [Validators.required, emailValidator]),
+    'passwords': new FormGroup({
+      'password': new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      'rePassword': new FormControl(),
+    }),
+    'tel': new FormControl(''),
+    'telRegion': new FormControl(''),
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  handleRegister(): void {
+
   }
 
 }
--- 08-Workshop-Forms/workshop/src/app/auth/util.ts
+++ 08-Workshop-Forms/workshop/src/app/auth/util.ts
@@ -3,6 +3,14 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 export function emailValidator(control: AbstractControl): ValidationErrors | null {
     const value = control.value;
 
+    // if (control.errors && Object.keys(control.errors).filter(errorName => errorName !== 'email').length > 0) {
+    //     return null;
+    // }
+
+    if (!value) {
+        return null;
+    }
+
     if (!/.{6,}@gmail\.(bg|com)/.test(value)) {
         return {
             email: true,
