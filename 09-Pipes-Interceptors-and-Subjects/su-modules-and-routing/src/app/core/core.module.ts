c6e32dc329c9e5c7d898244bdc915d2f2ffb7b18
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-04-01 17:49:48 +0300

    Add error interceptor

--- 09-Pipes-Interceptors-and-Subjects/su-modules-and-routing/src/app/core/core.module.ts
+++ 09-Pipes-Interceptors-and-Subjects/su-modules-and-routing/src/app/core/core.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Inject, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptor } from './token.interceptor';
+import { ErrorHandlerInterceptor } from './error-handler.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,12 +19,24 @@ import { TokenInterceptor } from './token.interceptor';
   exports: [
     HeaderComponent
   ],
-  providers:[
+  providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
       multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorHandlerInterceptor,
+      multi: true
     }
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Inject(HTTP_INTERCEPTORS) private interceptors: HttpInterceptor[]) {
+    // console.log(interceptors);
+    // for( const interceptor of this.interceptors){
+    //   interceptor.intercept();
+    // }
+  }
+}
