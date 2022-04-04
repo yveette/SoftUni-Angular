a483da817cc1c75eb9f089258dfa7b1c9403d16b
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-04-04 17:06:19 +0300

    Implement search for themes name.

--- 10-Workshop-Pipes-Interceptors-and-Subjects/workshop/src/app/feature/themes/theme-list/theme-list.component.ts
+++ 10-Workshop-Pipes-Interceptors-and-Subjects/workshop/src/app/feature/themes/theme-list/theme-list.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, filter, map, mergeMap, startWith, switchMap, tap } from 'rxjs/operators';
 import { ITheme } from '../../../core/interfaces';
 import { ThemeService } from '../../../core/theme.service';
 
@@ -11,13 +13,26 @@ export class ThemeListComponent implements OnInit, AfterViewInit {
 
   themeList: ITheme[];
 
+  searchControl = new FormControl('');
+  // new FormControl('', { updateOn: 'change' | 'blur' | 'submit' });
+
   constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
-    this.themeService.loadThemeList().subscribe(themeList => {
-      this.themeList = themeList;
-    });
+    this.searchControl.valueChanges.pipe(
+      // debounceTime(500), // use with mergeMap
+      filter(searchTerm => searchTerm.length > 3), // search for more than 3 characters
+      startWith(''),
+      tap(searchTerm => console.log(searchTerm)),
+      switchMap(searchTerm => this.themeService.loadThemeList(searchTerm))
+      // mergeMap => return the result from the observable
+      // switchMap => stop previous requests and takes the last
+    )
+      .subscribe(themeList => {
+        this.themeList = themeList;
+      });
   }
+
 
   ngAfterViewInit(): void {
     console.log('View was initialized');
